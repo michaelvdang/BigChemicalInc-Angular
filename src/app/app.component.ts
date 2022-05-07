a92d3fd9f9a77ab53f159cf24253db5aa221aa55
Michael Dang <mdang2023@gmail.com>
2022-05-07 02:00:38 -0700

    currently works with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  employee: any;
+  // employee = list;
+  constructor(private user:UsersService)
+  {
+    this.user.getDate().subscribe(data=>{
+      this.employee = data;
+      console.log(this.employee)
+    });
+  }
 }
---
+++ src/app/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http:HttpClient) { }
+
+  getDate()
+  {
+    let url = 'http://localhost:8000/employees/31';
+    // let url = 'https://jsonplaceholder.typicode.com/todos';
+    return this.http.get(url);//, {headers: {''}});
+  }
+}
